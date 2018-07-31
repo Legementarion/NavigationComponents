52b05300ba17fc322a5688a59ef359bb40f17bf7
Legementarion <legementario44>
2018-08-01 01:24:28 +0300

    mini nav

--- app/src/main/java/com/example/lego/myapplication/ColorNumberFragment.kt
+++ app/src/main/java/com/example/lego/myapplication/ColorNumberFragment.kt
@@ -2,14 +2,15 @@ package com.example.lego.myapplication
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_color_number.*
 
-private const val ARG_PARAM_COLOR = "color"
-private const val ARG_PARAM2 = "param2"
+const val ARG_PARAM_COLOR = "color"
+const val ARG_PARAM_AMOUNT = "amount"
 
 class ColorNumberFragment : Fragment() {
 
@@ -22,7 +23,13 @@ class ColorNumberFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.let {
-            fragmentContainer.setBackgroundColor( Color.parseColor(it.getString(ARG_PARAM_COLOR)))
+            fragmentContainer.setBackgroundColor(Color.parseColor(it.getString(ARG_PARAM_COLOR)))
+            textView.text = it.getString(ARG_PARAM_AMOUNT)
+        }
+
+        buttonForward.setOnClickListener {
+            val action = ColorNumberFragmentDirections.actionNotificationrFragmentToColorNumberFragment6()
+            Navigation.findNavController(it).navigate(action)
         }
     }
 
--- app/src/main/java/com/example/lego/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/lego/myapplication/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.lego.myapplication
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val finalHost = NavHostFragment.create(R.navigation.nav_main)
-
-//    override fun onSupportNavigateUp()
-//            = findNavController(R.id.my_nav_host_fragment).navigateUp()
+    val finalHost = NavHostFragment.create(R.navigation.nav_main)
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -35,10 +33,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager.beginTransaction()
                 .replace(R.id.my_nav_host_fragment, finalHost)
                 .commit()
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        finalHost.navController.addOnNavigatedListener{ navController: NavController, navDestination: NavDestination ->
+            val tab = navDestination.defaultArguments.getInt(ARG_PARAM_AMOUNT)
+            navigation.menu.getItem(tab).isChecked = true
+        }
     }
 }
